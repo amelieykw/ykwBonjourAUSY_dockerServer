c2e7b68c34aef2d955e4b23b490856e9fe1a0875
amelieykw <amelieykw1991@gmail.com>
2017-04-12 10:51:15 +0200

    add all the php scripts for inserting datas into mysql container and fetching datas from mysql container

--- images/app/src/public/index.php
+++ images/app/src/public/index.php
@@ -12,5 +12,11 @@
 		<a href="/create_table_rdv.php">create table rendez-vous</a><br/>
 		<br/>
 		<a href="/insert_table_site.php">insert data into table site</a><br/>
+		<a href="/insert_table_contact.php">insert data into table contact</a><br/>
+		<a href="/insert_table_rdv.php">insert data into table rendezvous</a><br/>
+		<br/>
+		<a href="/getAllSites.php">fetch data from table site</a><br/>
+		<a href="/getAllContact.php">fetch data from table contact</a><br/>
+		<a href="/getAllrdv.php">fetch data from table rendezvous</a><br/>
 	</body>
 </html>
